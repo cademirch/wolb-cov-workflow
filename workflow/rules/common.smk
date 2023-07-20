b6ad2aa0cec7fe5e776b8be3c493d58c366d417d
Cade <cmirchan@ucsc.edu>
2023-07-20 14:28:50 -0700

    add roles and titer calc

--- workflow/rules/common.smk
+++ workflow/rules/common.smk
@@ -6,14 +6,30 @@ from pathlib import Path
 from yaml import safe_load
 from snakemake.exceptions import WorkflowError
 
-for k, v in config["coverage_groups"].items():
-    if len(v) < 1:
-        raise (
-            WorkflowError(
+
+def validate_config():
+    for k, v in config["coverage_groups"].items():
+        if len(v) < 1:
+            raise WorkflowError(
                 f"Coverage group: `{k}` has no contigs listed, please correct this. "
             )
-        )
+        role_values = []
+        for j, r in config["roles"].items():
+            if isinstance(r, str):
+                role_values.append(r)
+            elif isinstance(r, list):
+                role_values.extend(r)
+            else:
+                raise TypeError(
+                    f"Wrong type for role '{j}', must be list or str. Found '{type(r)}'"
+                )
+        if k not in role_values:
+            raise WorkflowError(f"Coverage group: `{k}` has no role.")
 
+
+validate_config()
+
+# print(config["coverage_groups"].items())
 ## Read in sample sheets and resources yaml ##
 samples = pd.read_table(config["sample_sheet"], sep=",", dtype=str).replace(
     " ", "_", regex=True
--- workflow/scripts/parse_mosdepth.py
+++ workflow/scripts/parse_mosdepth.py
@@ -1,23 +1,29 @@
-"""
-Parses mosdepth summary output.
-"""
+import pandas as pd
 from pathlib import Path
-from collections import OrderedDict
 
 
-def parse(file: Path, groups: OrderedDict[str : list[str]]) -> OrderedDict[str:float]:
+def parse(file: Path, groups: dict[str : list[str]]) -> dict[str : str | str : float]:
     """
-    Parses mosdepth summary file and returns dict
-    where key is coverage group and value is avg coverage.
+    Parses mosdepth summary file. Reports mean depth for groups described in groups arg.
+
+    Args:
+        file (Path): Path to mosdepth summary file
+        groups (dict[str : list[str]]): Dict of coverage groups.
+                            Key is group name, value is list of contigs
+
+    Returns:
+        dict[str : str | str : float]: {"sample_id":samp, "group1":0.0}
     """
-    inverse_groups = OrderedDict()
-    out = OrderedDict()
+    sample_id = file.name.replace(".mosdepth.summary.txt", "")
+    inverse_groups = {}
+    out = {}
+    out["sample_id"] = sample_id
     for group, contigs in groups.items():
         out[group] = 0
         for contig in contigs:
             inverse_groups[contig] = group
 
-    with open(file, "r") as f:
+    with file.open() as f:
         for line in f:
             line = line.strip().split()
             chrom = line[0]
@@ -33,18 +39,37 @@ def parse(file: Path, groups: OrderedDict[str : list[str]]) -> OrderedDict[str:f
     return out
 
 
-def main():
-    groups = OrderedDict(snakemake.params["groups"])  # noqa: F821
-    infiles = snakemake.input  # noqa: F821
-    outfile = snakemake.output["csv"]  # noqa: F821
+def create_output(
+    df: pd.DataFrame, roles: dict[str : list[str]], outfile: Path
+) -> None:
+    """
+    Writes csv of mean depths and titers.
 
-    with open(outfile, "w") as out:
-        print("sample_id", *groups.keys(), sep=",", file=out)
-        for file in infiles:
-            file = Path(file)
-            sample_id = file.name.replace(".mosdepth.summary.txt", "")
-            cov_dict = parse(file, groups)
-            print(sample_id, *cov_dict.values(), sep=",", file=out)
+    Args:
+        df (pd.DataFrame): Dataframe with mean depth values
+        roles (_type_): Dict describing host/symbiont
+        outfile (Path): Path to output csv.
+    """
+    host = roles["host"]
+    symbionts = roles["symbionts"]
+    for symb in symbionts:
+        df[f"{symb}_titer"] = df[symb] / df[host]
+    df.to_csv(outfile, sep=",", index=False)
+
+
+def main():
+    groups = snakemake.params["groups"]  # noqa: F821
+    roles = snakemake.params["roles"]  # noqa: F821
+    infiles = snakemake.input  # noqa: F821
+    outfile = Path(snakemake.output["csv"])  # noqa: F821
+    depths = []
+
+    for file in infiles:
+        file = Path(file)
+        depths.append(parse(file, groups))
+
+    df = pd.DataFrame(depths)
+    create_output(df, roles, outfile)
 
 
 if __name__ == "__main__":
